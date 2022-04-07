bc9e4bf6cb960647f0a2237cd14048f7216aa4c4
sharkduy <buiduy1006@gmail.com>
2022-04-07 12:49:45 +0900

    add

---
+++ main.js
@@ -0,0 +1,18 @@
+//active menu button
+const toggle=document.getElementById('toggle');
+const navMenu=document.getElementById('MegaMenu');
+toggle.onclick=function(){
+    toggle.classList.toggle('active');
+    navMenu.classList.toggle('active');
+}
+
+//open login modal
+// const login=document.getElementById('Login');
+// const search=document.getElementById('opensearch');
+// login.onclick=function(){
+//     login.classList.toggle('active');
+// }
+// opensearch.onclick=function(){
+//     search.classList.toggle('active');
+// }
+
