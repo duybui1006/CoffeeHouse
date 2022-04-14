57cd840b65418d13a77e2845a7830fad1e41b0dd
sharkduy <buiduy1006@gmail.com>
2022-04-14 16:35:53 +0900

    update

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
-//active menu button
+//active Megamenu & mobile menu button
 const toggle=document.getElementById('toggle');
 const navMenu=document.getElementById('MegaMenu');
+const mobileMenu=document.getElementById('mobilemenu');
 toggle.onclick=function(){
     toggle.classList.toggle('active');
     navMenu.classList.toggle('active');
+    toggle.classList.toggle('show');
+    mobileMenu.classList.toggle('show');
 }
 
-//open login modal
-// const login=document.getElementById('Login');
-// const search=document.getElementById('opensearch');
-// login.onclick=function(){
-//     login.classList.toggle('active');
-// }
-// opensearch.onclick=function(){
-//     search.classList.toggle('active');
-// }
 
+//mobile config menu
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.height === "450px") {
+  dropdownContent.style.height = "0";
+  } else {
+  dropdownContent.style.height = "450px";
+  }
+  });
+}
+
+
+const tog=document.getElementById('toggle');
+const mbMenu=document.getElementById('mobilemenu');
+tog.onclick=function(){
+  tog.classList.toggle('active');
+  mbMenu.classList.toggle('active');
+}
+
+var mbconfig = document.getElementsByClassName("mobile-config");
+var j;
+
+for (j = 0; j < mbconfig.length; j++) {
+  mbconfig[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var mbconfigContent = this.nextElementSibling;
+  if (mbconfigContent.style.height === "600px") {
+    mbconfigContent.style.height = "0";
+  } else {
+    mbconfigContent.style.height = "600px";
+  }
+  });
+}
